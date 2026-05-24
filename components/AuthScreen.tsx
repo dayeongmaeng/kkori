b3c0f4ae1ffa493fd3e593ba436226887a3c1d34
dayeong maeng <dayeongmaeng@gmail.com>
2026-05-25 02:33:31 +0900

    feat: 로그인화면 문구 변경

--- components/AuthScreen.tsx
+++ components/AuthScreen.tsx
@@ -426,14 +426,14 @@ export default function AuthScreen() {
       <Animated.View style={[s.screen, { opacity: fadeAnim }]}>
         <View style={s.titleArea}>
           <Text style={s.headline}>꼬리에{'\n'}오신 것을 환영해요</Text>
-          <Text style={s.subhead}>특별한 날보다, 평범한 오늘을 남겨요{'\n'}더 많이 남겨둘걸 후회했던 마음에서 꼬리는 시작됐어요</Text>
+          <Text style={s.subhead}>특별한 날보다, 평범한 오늘을 남겨요{'\n'}더 많이 남겨둘걸 후회했던 마음에서,{'\n'}꼬리는 시작됐어요</Text>
         </View>
 
         <View style={s.logoArea}>
           <Animated.View style={[s.logo, logoTransform]}>
             <Image source={require('../assets/logo.png')} style={s.logoImage} contentFit="contain" />
           </Animated.View>
-          <Text style={s.logoCaption}>17년의 추억에서 시작된 기록 앱</Text>
+          <Text style={s.logoCaption}>17년의 시간을 기억하며</Text>
         </View>
 
         <View style={s.actions}>
